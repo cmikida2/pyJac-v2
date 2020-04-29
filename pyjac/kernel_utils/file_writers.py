4a95f94954930d26eedfc31e0c8b58a2e809493d
Cory Mikida <cmikida2@dunkel.cs.illinois.edu>
2020-04-29 16:45:18 -0500

    Fix issue when not building with OpenMP

--- pyjac/kernel_utils/file_writers.py
+++ pyjac/kernel_utils/file_writers.py
@@ -54,6 +54,7 @@ def get_header_preamble(lang):
  #warning 'OpenMP not found! Unexpected results may occur if using more than one \
 thread.'
  #define omp_get_num_threads() (1)
+ #define omp_get_thread_num() (0)
 #endif
 """.strip())]
 
